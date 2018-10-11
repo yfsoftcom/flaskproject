3c06e742964d4bdcf185d5494239f7856bac9d42
yfsoftcom <yfsoftcom@126.com>
2018-10-11 10:07:31 +0800

    add serval fields

--- apps/stock/logic/keep.py
+++ apps/stock/logic/keep.py
@@ -17,14 +17,21 @@ def get_profit():
     rows = []
     total = { 'amount': 0.0, 'profit': 0.0 }
     origin = { 'amount': 0.0, 'rate': 0.0 }
-    for stock in KEEP_STOCKS:
+    df = ts.get_realtime_quotes([ x[0] for x in KEEP_STOCKS])
+    print(df)
+    for i in range(0, len(KEEP_STOCKS)):
+        stock = KEEP_STOCKS[i]
         row = {}
         row['code'] = stock[0]
-        df = ts.get_realtime_quotes(stock[0])
+        # print(df)
         row['hand'] = stock[1]
         row['cost'] = stock[2]
-        row['name'] = df.name[0]
-        row['price'] = df.price[0]
+        row['name'] = df.name[i]
+        row['price'] = df.price[i]
+        row['pre_close'] = df.pre_close[i]
+        row['price_rate'] = round(( float(df.price[i]) - float(df.pre_close[i]) ) / float(df.pre_close[i]) * 100, 2)
+        row['open'] = df.open[i]
+        row['volume'] = int(int(df.volume[i]) / 100)
         # 总价
         row['amount'] = round(float(row['price']) * 100 * int(row['hand']), 2)
         # 收益， （现价-成本价） * 股数
