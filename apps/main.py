c31e704a614e90b43b5de422c9188313c3a2e252
yfsoftcom <yfsoftcom@126.com>
2020-01-13 22:02:49 +0800

    ignore xv & stock

--- apps/main.py
+++ apps/main.py
@@ -2,14 +2,14 @@
 from flask import Flask
 from libs.kit import *
 from xv_search.run import xv
-from worldcup2018.run import worldcup
-from stock.run import stock
+# from worldcup2018.run import worldcup
+# from stock.run import stock
 
 app = Flask(__name__)
 
 app.register_blueprint(xv, url_prefix='/xv')
-app.register_blueprint(worldcup, url_prefix='/worldcup')
-app.register_blueprint(stock, url_prefix='/stock')
+# app.register_blueprint(worldcup, url_prefix='/worldcup')
+# app.register_blueprint(stock, url_prefix='/stock')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5555, debug=True) 
